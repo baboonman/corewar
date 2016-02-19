15caaf6a965dfc1d50579239cff28ecc3b5e0d4a
Joseph RENOUF-GAY <jrenouf-@e1r4p3.42.fr>
2016-02-19 18:24:41 +0100

    struct proces

---
+++ vm/process/process_struct.h
@@ -0,0 +1,21 @@
+#ifndef PROCESS_STRUCT
+# define PROCESS_STRUCT
+
+typedef struct				s_ins
+{
+	int						opcode;
+	size_t					size;
+	int						param_val[3];
+	t_arg_type				param_type[3];
+}							t_ins;
+
+typedef struct				s_process
+{
+	int						reg[REG_NUMBER];
+	int						carry;
+	int						number_cycles;
+	size_t					pc;
+	t_ins					curr_ins;
+}							t_process;
+
+#endif
