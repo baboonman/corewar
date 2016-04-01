db9986dbcc95be835b8b5c5092bb245eeae10649
Victor WATRELOS-GUILBERT <vwatrelo@e1r4p2.42.fr>
2016-04-01 19:06:22 +0200

    Fix write with printf

--- src_vm/ncurses/update_vm_info.c
+++ src_vm/ncurses/update_vm_info.c
@@ -2,31 +2,16 @@
 
 void	update_vm_info(t_vm *vm)
 {
-	char	*str;
 	WINDOW	*win;
-	int		x;
-	int		y;
+	int		max_x;
+	int		max_y;
 
 	win = vm->ncurses.window[WIN_GLOB_INF];
-	mvwprintw(win, 0, 0, "Cycle to die: ");
-	if (!(str = ft_itoa(vm->cycle_die.step)))
-		return ;
-	getyx(win, y, x);
-	wprintw(win, SPACE_INT);
-	mvwprintw(win, y, x, str);
-	free(str);
-	mvwprintw(win, 1, 0, "Cycle delta: ");
-	if (!(str = ft_itoa(CYCLE_DELTA)))
-		return ;
-	getyx(win, y, x);
-	wprintw(win, SPACE_INT);
-	mvwprintw(win, y, x, str);
-	free(str);
-	mvwprintw(win, 2, 0, "Nb live: ");
-	if (!(str = ft_itoa(vm->cycle_die.tot_live)))
-		return ;
-	getyx(win, y, x);
-	wprintw(win, SPACE_INT);
-	mvwprintw(win, y, x, str);
-	free(str);
+	getmaxyx(win, max_y, max_x); 
+	mvwprintw(win, 0, 0, "% *c", max_x - 1, ' ');
+	mvwprintw(win, 0, 0, "Cycle to die: %d", vm->cycle_die.step);
+	mvwprintw(win, 1, 0, "% *c", max_x - 1, ' ');
+	mvwprintw(win, 1, 0, "Cycle delta: %d", CYCLE_DELTA);
+	mvwprintw(win, 2, 0, "% *c", max_x - 1, ' ');
+	mvwprintw(win, 2, 0, "Nb live: %d", vm->cycle_die.tot_live);
 }
